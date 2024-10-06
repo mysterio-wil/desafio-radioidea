dd3b02186c655f86b5eafa9195b1dbda4438ca2d
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 18:45:13 -0500

    Se creó y se implementó la clase Audio, se añadió sus atributos, se generó sus getters y setters dichos atributos, se creó el método meGusta(){} y el método reproducir(){}

---
+++ src/com/aluracursos/radioidea/modelos/Audio.java
@@ -0,0 +1,49 @@
+package com.aluracursos.radioidea.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducciones;
+    private int totalDeMeGusta;
+    private int clasificacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducciones() {
+        return totalReproducciones;
+    }
+
+    public void setTotalReproducciones(int totalReproducciones) {
+        this.totalReproducciones = totalReproducciones;
+    }
+
+    public int getTotalDeMeGusta() {
+        return totalDeMeGusta;
+    }
+
+    public void setTotalDeMeGusta(int totalDeMeGusta) {
+        this.totalDeMeGusta = totalDeMeGusta;
+    }
+
+    public int getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public void meGusta(){
+        this.totalDeMeGusta++;
+    }
+
+    public void reproducir(){
+        this.totalReproducciones++;
+    }
+
+}
