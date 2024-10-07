89b111300366825b07f9e4a1d7f7996426922861
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 20:21:34 -0500

    Se importaron las clases Cancion y Podcast, dentro del método main se intanció miCancion y miPodcast con la palabra clave 'new'.

--- src/com/aluracursos/radioidea/principal/Principal.java
+++ src/com/aluracursos/radioidea/principal/Principal.java
@@ -1,7 +1,16 @@
 package com.aluracursos.radioidea.principal;
 
+import com.aluracursos.radioidea.modelos.Cancion;
+import com.aluracursos.radioidea.modelos.Podcast;
+
 public class Principal {
     public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
 
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Gabriela Aguilar");
+        miPodcast.setTitulo("Café.Tech");
     }
 }
