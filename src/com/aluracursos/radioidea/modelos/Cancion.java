ec227f42b5bfd775f8ed341b8eac4cd67a64d590
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 19:10:24 -0500

    Se creó y se implementó la clase Cancion y se usó 'extends' a la clase Audio con sus atributos y se generó sus getters y setters de dichos atributos.

---
+++ src/com/aluracursos/radioidea/modelos/Cancion.java
@@ -0,0 +1,31 @@
+package com.aluracursos.radioidea.modelos;
+
+public class Cancion extends Audio {
+    private String album;
+    private String cantante;
+    private String genero;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
