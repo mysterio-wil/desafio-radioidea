eb3ffcb28ae518a0e7e742b3774e29514957c20d
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 20:45:14 -0500

    En la clase principal en el método main, se añádió dos bucles for uno para meGusta() y el otro reproducir()

--- src/com/aluracursos/radioidea/principal/Principal.java
+++ src/com/aluracursos/radioidea/principal/Principal.java
@@ -12,5 +12,15 @@ public class Principal {
         Podcast miPodcast = new Podcast();
         miPodcast.setPresentador("Gabriela Aguilar");
         miPodcast.setTitulo("Café.Tech");
+
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 5000; i++) {
+            miCancion.reproducir();
+        }
+
+        System.out.println("Total de reproducciones: " + miCancion.getTotalReproducciones());
+        System.out.println("Total de Me Gusta: " + miCancion.getTotalDeMeGusta());
     }
 }
