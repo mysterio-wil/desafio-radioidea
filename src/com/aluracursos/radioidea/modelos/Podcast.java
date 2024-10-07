7d4519e6a86858fb2ba2a116eac3d98748266e18
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-06 19:16:34 -0500

    Se creó y se implementó la clase Podcast y se usó 'extends' a la clase Audio con sus atributos y se generó sus getter y setter de dichos atributos.

---
+++ src/com/aluracursos/radioidea/modelos/Podcast.java
@@ -0,0 +1,31 @@
+package com.aluracursos.radioidea.modelos;
+
+public class Podcast extends Audio {
+    private String presentador;
+    private String descripcion;
+    private String categoria;
+
+    public String getPresentador() {
+        return presentador;
+    }
+
+    public void setPresentador(String presentador) {
+        this.presentador = presentador;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
